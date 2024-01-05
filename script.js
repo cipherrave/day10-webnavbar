d2ff24c945e25082bf14f8b29ca2f89eebee8fd3
cipherrave <arif_akmal@hotmail.com>
2024-01-05 17:16:46 +0800

    initial upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function openSideNav() {
+  document.getElementById("mobileList").style.display = "block";
+  document.getElementById("mobileList").style.transition =
+    "width 1s, height 1s";
+}
+
+function closeSideNav() {
+  document.getElementById("mobileList").style.display = "none";
+  document.getElementById("mobileList").style.transition =
+    "width 1s, height 1s";
+}
